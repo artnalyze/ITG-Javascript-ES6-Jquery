4659b136aee31ce4ba46b97337d294edaa76014f
Artnalyze <artnalyze@gmail.com>
2020-05-29 08:55:37 +0700

    Create karel.js

---
+++ Basic-js/exam/karel.js
@@ -0,0 +1,46 @@
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
+function main(){
+    //your code here
+    move();
+    move();
+    turnLeft();
+    //-------------
+    move();
+    move();
+    putBeeper();
+    //-------------
+    turnLeft();
+    turnLeft();
+    turnLeft();
+    //-------------
+    move();
+    move();
+    putBeeper();
+    //-------------
+    turnLeft();
+    //-------------
+    move();
+    move();
+    putBeeper();
+    //-------------
+    turnLeft();
+    //------------
+    move();
+    move();
+    putBeeper();
+    //-------------
+    move();
+    move();
+    putBeeper();
+    //-------------
+    turnLeft();
+    //-------------
+    move();
+    move();
+    putBeeper();
+ }
+ 
